f6352bff7a476500ef54b691c02e6c57757d2c38
RafPrataham <160498230+RafPrataham@users.noreply.github.com>
2024-03-31 15:21:29 +0700

    menambahkan fitur baru dari bot [nama_bot]

--- main.py
+++ main.py
@@ -13,6 +13,19 @@ if word in meme_dict.keys():
 else:
     # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
     print("Kata tidak ditemukan")
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
     
     
 # dictionary terdiri dari key: value
+# tugas kali ini menambahkan bot dengan function dari dokumentasi
